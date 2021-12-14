af6a0470c8d98c24a59ed5a7d20d4646fc83ea6c
joseb061 <hernandezbravojoseba@gmail.com>
2021-12-15 00:28:24 +0100

    Python

--- Python-quiniela/quiniela/io.py
+++ Python-quiniela/quiniela/io.py
@@ -6,27 +6,26 @@ import settings
 
 
 def load_matchday(season, division, matchday):
-    with sqlite3.connect(settings.DATABASE_PATH) as conn:
-        data = pd.read_sql(f"""
-            SELECT * FROM Matches
-                WHERE season = '{season}'
-                  AND division = {division}
-                  AND matchday = {matchday}
-        """, conn)
+    # with sqlite3.connect(settings.DATABASE_PATH) as conn:
+    #     data = pd.read_sql(f"""
+    #         SELECT * FROM Matches
+    #             WHERE season = '{season}'
+    #               AND division = {division}
+    #               AND matchday = {matchday}
+    #     """, conn)
+    data = pd.read_csv(r'provisional.csv', index_col = 0)
+    data = data[(data["season"] == season) & (data["division"] == division) & (data["matchday"] == matchday)]
     if data.empty:
         raise ValueError("There is no matchday data for the values given")
     return data
 
 
 def load_historical_data(seasons):
-    with sqlite3.connect(settings.DATABASE_PATH) as conn:
-        if seasons == "all":
-            data = pd.read_sql("SELECT * FROM Matches", conn)
-        else:
-            data = pd.read_sql(f"""
-                SELECT * FROM Matches
-                    WHERE season IN {tuple(seasons)}
-            """, conn)
+    if seasons == "all":
+        data = pd.read_csv(r'provisional.csv', index_col = 0)
+    else:
+        data = pd.read_csv(r'provisional.csv', index_col = 0)
+        data = data[data["season"] == seasons]
     if data.empty:
         raise ValueError(f"No data for seasons {seasons}")
     return data
