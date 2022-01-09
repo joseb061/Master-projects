46050ba0234972b310699b9d7c03651ccacd1318
joseb061 <hernandezbravojoseba@gmail.com>
2022-01-09 22:12:14 +0100

    Last version

--- Optimisation/GA/Readme.md
+++ Optimisation/GA/Readme.md
@@ -1,3 +1,22 @@
+# README.MD
+
+In this work we have tested the hypothesis that Andouine’s migration occurs with social copying
+using a genetic algorithm. The genetic algorithm has been designed for the minimisation of the
+fitness value defined in the first section and has given us a minimum result of 1.8M.
+
+![](Plots/years_fitnes.png)
+## Content
+
+The repository contains all the material necessary for the operation of the algorithm. 
+
+* binary.h : extension to generate binary numbers.
+
+* EDO_solver: extension to resolve and calculate the results of the differential equation. Requires the program "rk78/RKF78.c" which is inside the rk78 folder.
+
+*functions.h: contains all the necessary functions to generate the initial population and to generate the new populations.
+
+*main_functional.c : contains the main algorithm. From this program the rest of the extensions are called.
+
 ## Execution
 
 First of all, we will have to make several modifications to redirect the output file paths. to do this, we will have to enter to the following program and change the path of the output file:
--- Optimisation/GA/functions.h
+++ Optimisation/GA/functions.h
@@ -7,7 +7,7 @@
 
 #include "Randombits.h"
 #include "EDO_solver.h"
-#include "binary.h"
+//#include "binary.h"
 
 
 #define BETA 0.000024382635446
@@ -21,6 +21,45 @@
 #define nYears 12
 
 const double real[] = {15329., 14177., 13031., 9762, 11271, 8688., 7571., 6983., 4778., 2067., 1586., 793.};
+
+// Modified crossover to take into account width of parents and children
+void OnePointCrossover(unsigned long int p1, unsigned long int p2, unsigned long int *f1, unsigned long int *f2,unsigned  char width){
+	/* d \in [1, 8*sizeof(unsigned int)-1] */
+	unsigned char d = uniform()*(width-1) + 1;
+	unsigned long int mask = 0xFFFFFFFFFFFFFFFFU << d;
+	*f1 = (p1 & mask) | (p2 & ~mask);
+	*f2 = (p2 & mask) | (p1 & ~mask);
+}
+
+void TwoPointCrossover(unsigned int p1, unsigned int p2, unsigned int *f1, unsigned int *f2){
+    unsigned char len = 8*sizeof(unsigned int);
+    unsigned char p = uniform()*(len-2) + 1; /* p \in [1, len-2] */
+    /* We want a mask with p 0's at the beginning; len-q zeros at the end;
+    * and 1's between the positions p and q inclusive
+    * where q \in [p+1, len-1].
+    * CONSISTENCY NOTE: p <= len-2 ==> p+1 <= len-1
+    * THEN: mask = (0xFFFFFFFFU >> (len-q+p)) << p;
+    * OBSERVE THAT: p+1 <= len-q+p <= len-1 is a random number
+    * THUS: len-q+p = uniform()*(len-p-1) + p+1; */
+    unsigned int mask = (0xFFFFFFFFU >> (((unsigned char) uniform()*(len-p-1)) + p+1)) << p;
+    *f1 = (p1 & mask) | (p2 & ~mask);
+    *f2 = (p2 & mask) | (p1 & ~mask);
+}
+
+void UniformCrossover(unsigned int p1, unsigned int p2, unsigned int *f1, unsigned int *f2, double prob){
+    unsigned char len = 8*sizeof(*f1);
+    unsigned int mask = 0U;
+    register unsigned char i;
+    for(i=0; i < len; i++) if(uniform() < prob) mask = mask | (1U << i);
+    *f1 = (p1 & mask) | (p2 & ~mask);
+    *f2 = (p2 & mask) | (p1 & ~mask);
+}
+
+void BitFlipMutation(unsigned long int *f, double prob, unsigned char width){
+	register unsigned char i;
+	//Only allowed to flip bits for the given width. Otherwise it will change bits that are out of range for a given parameter
+	for(i=0; i < width; i++) if(uniform() < prob) *f = (*f)^(1U << i);
+}
 
 void ExitError(const char *miss, int errcode) {
     fprintf (stderr, "\nERROR: %s.\nStopping...\n\n", miss); exit(errcode);
